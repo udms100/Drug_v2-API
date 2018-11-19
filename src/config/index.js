98c09286f3e6e5255578a96630eba8fdec394faf
udms100 <infoxfinesnig@gmail.com>
2018-11-19 10:13:32 +0100

    set up

--- src/config/index.js
+++
@@ -1,6 +0,0 @@
-export default {
-  "port": 3005,
-  "mongoUrl": "mongodb://localhost:27017/foodtruck-api",
-  "bodyLimit": "100kb"
-  }
-  
--- src/db.js
+++ src/db.js
@@ -1,7 +1,1 @@
-import mongoose from 'mongoose';
-import config from './config';
 
-export default callback => {
-  let db = mongoose.connect(config.mongoUrl);
-  callback(db);
-}
