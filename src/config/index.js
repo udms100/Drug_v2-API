2ca05a1200aac093c5d0507d9c128f21ca156a56
udms100 <infoxfinesnig@gmail.com>
2018-12-03 11:49:34 +0100

    remove src

--- src/config/index.js
+++
@@ -1,6 +0,0 @@
-export default{
-    "port": 4000,
-    "mongoUrl": "mongodb://localhost:27017/DRUG_v1-API",
-    "Limit": "100kb"
-
-}
--- src/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-import config from './config';
-
-export default callback => {
-    let db = mongoose.connect(config.mongoUrl);
-    callback(db);
-}
